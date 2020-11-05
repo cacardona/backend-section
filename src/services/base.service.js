aacfd8964e239e8b749a23e6e897f771f17a9fe9
carlos <ccardonat@hotmail.com>
2020-11-04 21:20:25 -0500

    [ADD] services added

---
+++ src/services/base.service.js
@@ -0,0 +1,61 @@
+
+
+class BaseService {
+    constructor(repository){
+        this.repository = repository;
+    }
+
+
+async get(id) {
+    if(!id){
+        const error = new Error();
+        error.status = 400;
+        error.message ="id must be sent";
+        throw error;
+    }
+
+    const currenEntity = await this.repository.get(id);
+
+    if(!currenEntity) {
+        const error = new Error();
+        error.status = 404;
+        error.message = "entity does nof found";
+        throw error;
+
+    }
+
+    return currenEntity;
+ }
+
+ async getAll(){
+     return await this.repository.getAll();
+ }
+
+ async create(entity){
+    return await this.repository.create(entity);
+ }
+
+ async update(id, entity){
+     if(!id){
+         const error = new Error();
+         error.status = 400;
+         error.message = "id must be sent";
+         throw error;
+     }
+
+     return await this.repository.update(id, entity);
+ }
+
+ async delete(id){
+     if(!id){
+        const error = new Error();
+        error.status = 400;
+        error.message = "id must be sent";
+        throw error;
+     }
+     return await this.repository.delete(id);
+ }
+    
+}
+
+module.exports = BaseService;
--- src/startup/container.js
+++ src/startup/container.js
@@ -6,7 +6,12 @@ const config = require("../config");
 const app = require(".");
 
 //services
-const { HomeService } = require("../services");
+const { 
+    HomeService,
+     UserService,
+      IdeaService,
+       CommentService
+     } = require("../services");
 
 //controllers
 const { HomeController } = require("../controllers");
@@ -34,7 +39,10 @@ container
     config: asValue(config)
 })
 .register({
-    HomeService: asClass(HomeService).singleton()
+    HomeService: asClass(HomeService).singleton(),
+    UserService: asClass(HomeService).singleton(),
+    CommentService: asClass(HomeService).singleton(),
+    IdeaService: asClass(HomeService).singleton()
 })
 .register({
     HomeController: asClass(HomeController.bind(HomeController)).singleton()   //se pone bind para que express no cambie el scope
