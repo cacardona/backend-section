b3dba16ed8368de795c9126e596c5fddf64b97b5
carlos <ccardonat@hotmail.com>
2020-11-15 21:34:51 -0500

    [ADD] tests added

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+    user: {
+      _id: "5fab653001adee36f8b71645",
+      name: "Marluan",
+      username: "Marluan03",
+      password: "mystrongPassword"
+    },
+    users: [
+      {
+        _id: "507f191e810c19729de860ea",
+        name: "Marluan",
+        username: "Marluan03",
+        password: "mystrongPassword"
+      },
+      {
+        _id: "507f191e810c19729de860eb",
+        name: "Erick",
+        username: "Erick_34",
+        password: "mystrongPassword"
+      }
+    ]
+  };
